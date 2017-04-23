7db733cf87eaf5a38d4b7d36e74b60b7fac3f5e3
Alex Lee <doomcat9167@gmail.com>
2017-04-22 18:54:40 -0700

    body json string now parsed into JSON object

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sms = require('./public/js/sms.js');
+var request = require('request');
+var translateObject = require('./public/js/translate.js');
 
 var router = express.Router();
 
 //THIS IS SO YOU CAN TAKE HTML FORM DATA INTO APP.JS
 app.use(express.static('public'));
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies
 
 // // DAT MIDDLEWARE SHIT
 // //First middleware before response is sent
@@ -33,76 +37,64 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //ROUTES FOR HTML
 app.get('/', function(req, res) {
-    res.sendfile('./public/index.html');
+  res.sendfile('./public/index.html');
 });
 
 app.get('/dashboard', function(req, res) {
-    res.sendfile('./public/dashboard.html');
+  res.sendfile('./public/dashboard.html');
 });
 
 app.get('/call', function(req, res) {
-    res.sendfile('./public/call.html');
+  res.sendfile('./public/call.html');
 });
 
 app.post('/call', function(req, res) {
-    var phoneNumber = req.body.phone_number;
-    console.log("Your Number" + req.body.phone_number);
-    res.sendfile('./public/index.html');
+  var phoneNumber = req.body.phone_number;
+  console.log("Your Number" + req.body.phone_number);
+  res.sendfile('./public/index.html');
 });
 
+//
+// app.post('/message', function(req, res) {
+//   var message = req.body.message;
+//   sms.sendSms(message);
+//   translateObject.translate(message);
+//   console.log(message);
+//
+//   res.sendfile('./public/index.html');
+// });
 
-app.post('/message', function(req, res) {
-    var message = req.body.message;
-    sms.sendSms(message);
-    console.log(message);
-
-    res.sendfile('./public/index.html');
+app.get('/translate', function(req, res) {
+  var jsonObject = request('http://www.transltr.org/api/translate?text=what%20is%20your%20name%3F&to=ko', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var formattedJSON = JSON.parse(body); //Turn dat string into JSON
+      console.log(formattedJSON.translationText);
+    }
+  }).pipe(res);
 });
 
 app.get('/schedule', function(req, res) {
-    res.sendfile('./public/schedule.html');
+  res.sendfile('./public/schedule.html');
 });
 
-//grab ID from url
-app.get('/:id', function(req, res){
-    res.send('Current ID: ' + req.params.id);
-});
+// //grab ID from url
+// app.get('/:id', function(req, res) {
+//   res.send('Current ID: ' + req.params.id);
+// });
 
-
-
-
-//nested routing
-app.get('/:name/:id', function(req, res){
-    res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
-});
-
-//regex NEED the id to be five digits long number.
-app.get('/things/:id([0-9]{5})', function(req, res){
-    res.send('id: ' + req.params.id);
-});
 
 //ANY OTHER ROUTES, HANDLE ERROR
-app.get('*', function(req, res){
-    res.send('Sorry, this is an invalid URL.');
+app.get('*', function(req, res) {
+  res.send('Sorry, this is an invalid URL.');
 });
 
 //Simple request time logger, displays in console of request times.
-app.use(function(req, res, next){
-	console.log("A new request received at " + Date.now());
-	//This function call is very important. It tells that more processing is
-	//required for the current request and is in the next middleware function/route handler.
-	next();
+app.use(function(req, res, next) {
+  console.log("A new request received at " + Date.now());
+  //This function call is very important. It tells that more processing is
+  //required for the current request and is in the next middleware function/route handler.
+  next();
 });
-
-// http.createServer(function (req, res) {
-//     //Create TwiML response
-//     var twiml = new twilio.TwimlResponse();
-//     twiml.say("Hello from your pals at Twilio! Have fun.");
-//
-//     res.writeHead(200, {'Content-Type': 'text/xml'});
-//     res.end(twiml.toString());
-//
-// }).listen(1337, '127.0.0.1');
 
 
 
---
+++ public/js/translate.js
@@ -0,0 +1,8 @@
+// var request = require('request');
+//
+//
+// module.exports.translate = function(message) {
+//   request('http://www.transltr.org/api/translate?text=what%20is%20your%20name%3F&to=ko', function(err, body) {
+//     res.json(body);
+//   });
+// }
